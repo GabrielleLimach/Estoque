0e8109ebbc952543a0309c903dfcce641eec746c
Gabrielle Limach <gabrielle.lmm@gmail.com>
2018-07-16 14:06:11 -0300

    alteracoes

--- src/main/java/com/solicitacoes/seap/repository/MaterialRepository.java
+++ src/main/java/com/solicitacoes/seap/repository/MaterialRepository.java
@@ -1,7 +1,16 @@
 package com.solicitacoes.seap.repository;
 
 import com.solicitacoes.seap.models.Material;
+import com.solicitacoes.seap.models.MaterialTipo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Set;
+
 public interface MaterialRepository extends JpaRepository <Material, Long> {
+
+    List<Material> findByQuantidade(int quantidade);
+
+    List<Material> findByFkmaterialtipo(MaterialTipo fkmaterialtipo);
+
 }
--- src/main/java/com/solicitacoes/seap/resource/MaterialResource.java
+++ src/main/java/com/solicitacoes/seap/resource/MaterialResource.java
@@ -1,17 +1,25 @@
 package com.solicitacoes.seap.resource;
 
 import com.solicitacoes.seap.models.Material;
+import com.solicitacoes.seap.models.MaterialTipo;
 import com.solicitacoes.seap.repository.MaterialRepository;
+import org.hibernate.annotations.Persister;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.flyway.FlywayDataSource;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.net.URI;
+import java.util.Collections;
 import java.util.List;
 
 @RestController
@@ -22,12 +30,32 @@ public class MaterialResource {
     @Autowired
     private MaterialRepository materialRepository;
 
+    //retorna uma lista com todos os materiais
     @GetMapping
     private List<Material> listar(){
 
         return materialRepository.findAll();
     }
 
+
+    //retorna uma lista de materiais de acordo com a quantidade informada
+    @GetMapping("/qde/{quantidade}")
+    private List<Material> buscarPeloTipo(@PathVariable("quantidade") int id){
+
+        return  materialRepository.findByQuantidade(id);
+
+    }
+
+    //retorna uma lista de materiais de acordo com o tipo informado
+    @GetMapping("/tipo/{fkmaterialtipo}")
+    private List<Material> buscarPeloTipo(@PathVariable("fkmaterialtipo") MaterialTipo id){
+
+        return  materialRepository.findByFkmaterialtipo(id);
+
+    }
+
+
+    //insere um novo material, e retorna o material recem inserido
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     private ResponseEntity<Material> criar (@RequestBody Material material, HttpServletResponse response){
@@ -40,19 +68,23 @@ public class MaterialResource {
         return ResponseEntity.created(uri).body(materialSalvo);
     }
 
-    @GetMapping("/idmaterial")
+    //retorna uma material especifico a partir do id (codigo) do material
+    @GetMapping("/{idmaterial}")
     private ResponseEntity<Material> buscarPeloCodico(@PathVariable("idmaterial") Long id){
+
         return  ResponseEntity.ok(materialRepository.findById(id).get());
 
     }
 
-    @DeleteMapping("/idmaterial")
+    //deleta pelo id (codigo) o material informado
+    @DeleteMapping("/{idmaterial}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     private void remover(@PathVariable("idmaterial")Long id){
         Material materialSalvo = materialRepository.findById(id).get();
         materialRepository.delete(materialSalvo);
     }
 
+    //alterar os dados de um material ja cadastrado
     @PutMapping("/idmaterial")
     private ResponseEntity<Material> atualizar(@PathVariable("idmaterial")Long id, @Valid @RequestBody Material material){
         Material materialSalvo = materialRepository.findById(id).get();
