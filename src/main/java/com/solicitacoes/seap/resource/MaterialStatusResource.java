f45a8c7dcb9c8d112a833c2a3db7202f57086e73
Gabrielle Limach <gabrielle.lmm@gmail.com>
2018-07-20 11:39:52 -0300

    classes de classe resources

---
+++ src/main/java/com/solicitacoes/seap/resource/MaterialStatusResource.java
@@ -0,0 +1,13 @@
+package com.solicitacoes.seap.resource;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/materialstatus")
+@CrossOrigin("*")
+public class MaterialStatusResource {
+
+}
---
+++ src/main/java/com/solicitacoes/seap/resource/SolicitacaoStatusResource.java
@@ -0,0 +1,14 @@
+package com.solicitacoes.seap.resource;
+
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/solicitacaostatus")
+@CrossOrigin("*")
+public class SolicitacaoStatusResource {
+
+
+}
