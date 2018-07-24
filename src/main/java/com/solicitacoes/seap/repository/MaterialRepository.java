dd62aaf514e3fdcfdb8801b1f9a8295884bfc59b
Gabrielle Limach <gabrielle.lmm@gmail.com>
2018-07-24 08:31:08 -0300

    falteracoes

--- src/main/java/com/solicitacoes/seap/repository/MaterialRepository.java
+++ src/main/java/com/solicitacoes/seap/repository/MaterialRepository.java
@@ -1,15 +1,10 @@
 package com.solicitacoes.seap.repository;
 
 import com.solicitacoes.seap.models.Material;
-<<<<<<< HEAD
 import com.solicitacoes.seap.models.MaterialTipo;
-=======
-import com.solicitacoes.seap.resource.HistoricoResource;
->>>>>>> cdb7df224fde77ab9c4caf38a8a206bb0b0cd47a
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
-import java.util.Set;
 
 public interface MaterialRepository extends JpaRepository <Material, Long> {
 
